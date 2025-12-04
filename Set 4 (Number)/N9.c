71e29f712937bf7c2efb07edbed8b6d12e23c7cf
shubendu7 <shubendudas265@gmail.com>
2025-12-04 17:48:30 +0530

    COMMIT

--- Set 4 (Number)/N9.c
+++ Set 4 (Number)/N9.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 int main(){
     int f1=1,f2=2,sum=0;
-    int i;
+    int i=4;
     int N;
     printf("Enter the N terms : ");
     scanf("%d",&N);
     printf("\nThe non fibo numbers are : ");
-    for(i=4;i<=N;i++){
+    while(N){
         sum=f1+f2;
         if(sum!=i)
         printf("%d ",i);
         f1=f2;
         f2=sum;
+        i++;
+        N--;
     }
     return 0;
 }
