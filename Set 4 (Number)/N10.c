9a5b67ab868c3467e48fa3550781d304f09d7830
shubendu7 <shubendas265@gmail.com>
2026-03-19 20:36:50 +0530

    Final

--- Set 4 (Number)/N10.c
+++ Set 4 (Number)/N10.c
@@ -1,19 +1,32 @@
 #include<stdio.h>
 int main(){
-    int f1=0,f2=1,sum=0;
-    int Fibo[100]={[0]=0,[1]=1};
-    int i=2;
+    int f1=0,f2=1,next=0;
     int M,N;
     printf("Enter the M & N Range Values : ");
     scanf("%d %d",&M,&N);
-    printf("\nThe Sum of Fibo numbers : ");
-    while(M!=N){
-        sum=f1+f2;
-        Fibo[i]=sum;
-        f1=f2;
-        f2=sum;
-        i++;
-        M++;
+    printf("\nFibonacci series in the range [%d, %d]: ", M, N);
+    
+    // Handle edge case when M is 0 or 1
+    if(M <= 0 && 0 <= N){
+        printf("0 ");
     }
+    if(M <= 1 && 1 <= N){
+        printf("1 ");
+    }
+    
+    // Generate and print Fibonacci numbers within range
+    while(1){
+        next = f1 + f2;
+        f1 = f2;
+        f2 = next;
+        
+        if(next > N){
+            break;
+        }
+        if(next >= M && next <= N){
+            printf("%d ", next);
+        }
+    }
+    printf("\n");
     return 0;
 }
